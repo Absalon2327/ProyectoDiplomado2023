20f5fd6a4e99b69b01535bf1d52c2bb44f84d73b
JosDCZ <90920993+JosDCZ@users.noreply.github.com>
2023-10-18 14:31:37 -0600

    Correcciones de ortografia 1

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -161,7 +161,7 @@ export class TopbarComponent implements OnInit {
       icon: 'question',
       title: "¿Desea cerrar la sesión?",
       showDenyButton: true,
-      confirmButtonText: "Cerrar la",
+      confirmButtonText: "Cerrar",
       confirmButtonColor: "#972727",
       denyButtonText: `Cancelar`,
       denyButtonColor: "#2c3136",
